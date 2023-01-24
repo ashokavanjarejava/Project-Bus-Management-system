8128e9dba5156b065b21a57d3e121f559bc76587
rbubus <115520431+rbubus@users.noreply.github.com>
2023-01-24 21:17:38 +0530

    Add files via upload

---
+++ rbu_eureka/src/main/java/com/rbu/eureka/RbuEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.rbu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbuEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbuEurekaApplication.class, args);
+	}
+
+}
---
+++ rbubus-admin-api/src/main/java/com/rbu/eureka/RbubusAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.rbu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbubusAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbubusAdminApiApplication.class, args);
+	}
+
+}
